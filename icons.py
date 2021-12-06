dd6d86738685d88054b85fa85c330e25f8fe1590
MaxIvanyshen <maxivanyshen@gmail.com>
2021-12-06 23:52:20 +0200

    add config

---
+++ icons.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+# You can define a letter and it's icon here
+group_icons = ["SYS",
+               "DEV",
+               "BROWSE ",
+               "MUSIC"
+               ]
+
